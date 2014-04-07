0b959509226b65b0775f8e3cb52f41f0d8f702d8
Brian Keane <lonesomewhistle@gmail.com>
2014-04-07 15:41:53 -0500

    Adding Sign Up to Server

--- server.rb
+++ server.rb
@@ -14,6 +14,26 @@ get '/sign_up' do
   erb :sign_up
 end
 
+post '/sign_up' do
+  @email = params[:email]
+  @password = params[:password]
+  @full_name = params[:full_name]
+  @birthdate = Time.at(params[:birthdate])
+  @phone = params[:phone]
+  result = DMR.SignUp.run({ email: @email, password: @password, birthdate: @birthdate,
+                  full_name: @full_name, phone: @phone })
+  if result.success?
+    erb :sign_up_success
+  elsif result.error == :email_taken
+    @error_message = "That email is already taken.  Please Try Again. "
+    erb :sign_up_error
+  end
+end
+
+
+
+end
+
 get '/sign_in' do
   erb :sign_in
 end
