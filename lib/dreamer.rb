1b7cc95356ef7550162a04a7f68914fb69ec3c8d
Ronak Patel <patel66@sbcglobal.net>
2014-04-07 11:51:41 -0500

    updated dreamer.rb to include journal_entry.rb

--- lib/dreamer.rb
+++ lib/dreamer.rb
@@ -2,7 +2,7 @@ module DMR
 end
 
 require_relative "dreamer/user.rb"
-require_relative "dreamer/journal.rb"
+require_relative "dreamer/journal_entry.rb"
 require_relative "dreamer/db.rb"
 require_relative "dreamer/sleep_entry.rb"
 require_relative "dreamer/session.rb"
